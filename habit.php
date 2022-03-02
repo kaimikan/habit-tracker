72fe798a651d6b8b9980ab7a1924c1b8310f3c9f
kaimikan <oihic@protonmail.com>
2022-03-02 19:47:10 +0100

    Connecting Form to Local Database

---
+++ habit.php
@@ -0,0 +1,31 @@
+<?php
+
+if(isset($_POST['name']))
+{
+	$con = mysqli_connect('localhost', 'root', '', 'habit_tracker');
+
+	$name = $_POST['name'];
+	$description = $_POST['description'];
+	$start_date = $_POST['start_date'];
+	$end_date = $_POST['end_date'];
+	$is_everyday = isset($_POST['publishok']);	
+	$min_times = $_POST['min_reps'];
+	$max_times = $_POST['max_reps'];
+	$build_up_amount = $_POST['rep_build_up'];
+	$bounty = $_POST['bounty'];
+
+	$sql = "INSERT INTO `habits`(`name`, `description`, `start_date`, `end_date`, `is_everyday`, `min_times`, `max_times`, `build_up_amount`, `bounty`) VALUES ('$name', '$description', '$start_date', '$end_date', '$is_everyday', '$min_times', '$max_times', '$build_up_amount', '$bounty')";
+
+	$rs = mysqli_query($con, $sql);
+
+	if($rs)
+	{
+		echo "Habit Insterted";
+	}
+}
+else
+{
+	echo "Error";
+	
+}
+?>
