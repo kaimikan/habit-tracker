4643381568fd846f383be5fc6f1599704b199085
kaimikan <oihic@protonmail.com>
2022-05-05 22:38:02 +0200

    Base backend & split frontend

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+// helmet helps make requests to server secure
+const helmet = require("helmet");
+// morgan helps log server requests
+const morgan = require("morgan");
+// used for uploading files (not a good idea practice to do it in the app but this is done for example purposes - usually files are hosted on AWS or something similar)
+const multer = require("multer");
+// used to fetch uploaded files from server
+const path = require("path");
+
+dotenv.config();
+mongoose.connect(
+  process.env.MONGO_URL,
+  {
+    /* options */
+  },
+  (err) => {
+    if (err) console.log(err);
+    else console.log("MongoDB is connected");
+  }
+);
+
+// MIDDLEWARE
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log("Backend server is running!");
+});
